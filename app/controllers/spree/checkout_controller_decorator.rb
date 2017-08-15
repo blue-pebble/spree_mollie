814c510a76b2fbf87884868b193bfc58504b4b01
Thomas Lepelaars <thomas.lepelaars@gmail.com>
2017-08-15 19:06:50 +0200

    Update checkout_controller_decorator.rb

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -9,19 +9,19 @@ module Spree
       return unless params[:state] == "payment"
 
       # check to see if there is an existing mollie payment pending
-      mollie_payment_method = PaymentMethod.find_by(type: 'Spree::PaymentMethod::MolliePayment')
+      mollie_payment_method = PaymentMethod.find_by(type: 'Spree::Gateway::MolliePayment')
       payment = @order.payments.valid.where(payment_method: mollie_payment_method).first
 
       begin
         mollie = Mollie::API::Client.new.(MOLLIE_API_KEY)
-        mollie.setApiKey mollie_payment_method.preferred_api_key
         mollie_payment = mollie.payments.get(payment.source.transaction_id) if payment
 
         unless payment && mollie_payment && ['open','pending'].include?(mollie_payment.status)
           mollie_payment = mollie.payments.create \
             :amount       => @order.total,
             :description  => "Payment for order #{@order.number}",
-            :redirectUrl  => mollie_url(@order, :utm_nooverride => 1), # ensure that transactions are credited to the original traffic source
+            :redirect_url  => mollie_url(@order, :utm_nooverride => 1), # ensure that transactions are credited to the original traffic source
+            :webhook_url => mollie_url(@order, :utm_nooverride => 1),
             :method       => params[:order][:payments_attributes][0][:payment_method_id],
             # :billingEmail => @order.email, # when email is provided, Mollie sends an email with payment details (e.g. for banktransfer)
             :metadata     => {
@@ -35,7 +35,7 @@ module Spree
             :status => mollie_payment.status,
             :amount => mollie_payment.amount,
             :description => mollie_payment.description,
-            :created_at => mollie_payment.createdDatetime
+            :created_at => mollie_payment.created_datetime
           }
           if mollie_payment.method == 'banktransfer'
             source_params[:banktransfer_bank_name] = mollie_payment.details.bankName
@@ -50,7 +50,7 @@ module Spree
           })
         end
 
-        redirect_to mollie_payment.getPaymentUrl and return
+        redirect_to mollie_payment.payment_url and return
       rescue Mollie::API::Exception => e
         logger.debug << "Mollie API call failed: " << (CGI.escapeHTML e.message)
       end
